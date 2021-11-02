e02e29c588a04ea953cebd395cda51e136e9475d
namya009 <namyapatel02@gmail.com>
2021-11-02 13:41:52 -0400

    model updated!!

---
+++ Assignment1/src/main/java/ca/gbc/comp3095/comp3095assignment1/model/CookingMethod.java
@@ -0,0 +1,23 @@
+package ca.gbc.comp3095.comp3095assignment1.model;
+
+public class CookingMethod {
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    private String method;
+}
---
+++ Assignment1/src/main/java/ca/gbc/comp3095/comp3095assignment1/model/Ingredient.java
@@ -0,0 +1,41 @@
+package ca.gbc.comp3095.comp3095assignment1.model;
+
+public class Ingredient {
+    private String name;
+    private Long id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Unit getUnit() {
+        return unit;
+    }
+
+    public void setUnit(Unit unit) {
+        this.unit = unit;
+    }
+
+    private String description;
+    private Unit unit;
+}
