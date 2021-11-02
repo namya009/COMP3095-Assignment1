f36fd9d330eef1f77ef358a1b7704a1afb4800fa
namya009 <namyapatel02@gmail.com>
2021-11-02 06:46:13 -0400

    project created!!

---
+++ Assignment1/src/main/java/ca/gbc/comp3095/comp3095assignment1/Comp3095Assignment1Application.java
@@ -0,0 +1,13 @@
+package ca.gbc.comp3095.comp3095assignment1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Comp3095Assignment1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Comp3095Assignment1Application.class, args);
+    }
+
+}
---
+++ Assignment1/src/test/java/ca/gbc/comp3095/comp3095assignment1/Comp3095Assignment1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.gbc.comp3095.comp3095assignment1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Comp3095Assignment1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
