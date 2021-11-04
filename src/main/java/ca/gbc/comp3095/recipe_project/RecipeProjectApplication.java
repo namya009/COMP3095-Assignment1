c6f59135969f4240ebc1b3321d3f5a8ac04b800f
pruthvisoni1604 <pruthvisoni1604@gmail.com>
2021-11-04 16:31:27 -0400

    re init2

---
+++ src/main/java/ca/gbc/comp3095/recipe_project/RecipeProjectApplication.java
@@ -0,0 +1,13 @@
+package ca.gbc.comp3095.recipe_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeProjectApplication.class, args);
+    }
+
+}
